ff6d3ad61ad0526b0bf0984eb3bf256d89d83f6d
Matt Lissner <matt@doejo.com>
2018-05-17 14:16:47 -0500

    css

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link href="https://fonts.googleapis.com/css?family=Lato|Roboto" rel="stylesheet">
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-85987965-1', 'auto');
+	ga('send', 'pageview');
+
+	</script>
 	<?php wp_head(); ?>
 </head>
 <body  <?php body_class() ?> >
